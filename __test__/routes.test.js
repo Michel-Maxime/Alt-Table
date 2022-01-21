608fff18d1e56457165ec270eca334a0410c5602
Ziirkov <55087514+Ziirkov@users.noreply.github.com>
2022-01-21 17:54:01 +0100

    Test des routes
    4 premières US

---
+++ __test__/routes.test.js
@@ -0,0 +1,43 @@
+const request = require('supertest')
+
+describe("Meals Routes", () => {
+
+    it('test /meals GET', async () => {
+        const res = await request("localhost:8000").get('/meals');
+        expect(res.status).toBe(200)
+    });
+
+    it('test /meals POST', async () => {
+        const res = await request("localhost:8000")
+            .post("/meals")
+            .send(
+                {
+                    name: 'Added Meal 55',
+                    description: 'Id test meal',
+                    type: 'plat principal',
+                    price: 15,
+                    quantity: 0
+                }
+            )
+        expect(res.status).toBe(200);
+    })
+
+    it('test /meals PUT', async () => {
+        const res = await request("localhost:8000")
+            .put("/meals/61e97f18e34305a0c9fcf4be")
+            .send(
+                {
+                    quantity: 10
+                }
+            )
+        expect(res.status).toBe(200);
+    })
+
+})
+
+describe("Menu Routes", () => {
+    it('test /menu GET', async () => {
+        const res = await request("localhost:8000").get('/menu');
+        expect(res.status).toBe(200)
+    });
+}) 
