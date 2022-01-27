d900be1165bbe8a2cdd2cacac0ef8efd47f9fbe6
Ziirkov <55087514+Ziirkov@users.noreply.github.com>
2022-01-27 11:27:08 +0100

    Retrait console.log

--- __tests__/mealsSchema.test.js
+++ __tests__/mealsSchema.test.js
@@ -1,15 +1,14 @@
 const mongo = require('../repository/mongoConnection')
 const Meal = require('../repository/models/mealSchema')
 
-describe('Meals Schema',()=>{
+describe('Meals Schema', () => {
     let newMeal = {
-        name : "Pizza",
-        description:"Pizza romaine",
-        price:10,
-        quantity:5
+        name: "Pizza",
+        description: "Pizza romaine",
+        price: 10,
+        quantity: 5
     }
-    console.log(newMeal.name)
-    beforeAll(async()=>{
+    beforeAll(async () => {
         mongo.connection
     })
 
@@ -17,16 +16,16 @@ describe('Meals Schema',()=>{
         await mongo.connection.close();
     });
 
-    test('Should correctly validate a meal',async ()=>{
+    test('Should correctly validate a meal', async () => {
         await expect(new Meal(newMeal).validate()).resolves.toBeUndefined()
     })
 
-    test('Should throw a error if price is not a number',async ()=>{
+    test('Should throw a error if price is not a number', async () => {
         newMeal.price = "Not a number"
         await expect(new Meal(newMeal).validate()).rejects.toThrow()
     })
 
-    test('Should throw a error if quantity is not a number',async ()=>{
+    test('Should throw a error if quantity is not a number', async () => {
         newMeal.quantity = "Not a number"
         await expect(new Meal(newMeal).validate()).rejects.toThrow()
     })
